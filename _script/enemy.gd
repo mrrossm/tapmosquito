01a83156c6a6b9eabf55ad23e12d0f0e4a55f7e5
ross <no>
2024-04-25 09:11:30 +0800

    sound design nearly finished

--- _script/enemy.gd
+++ _script/enemy.gd
@@ -3,15 +3,20 @@ extends Node2D
 signal is_tapped
 signal not_tapped
 
+@onready var tapped_sound = $TappedSound
+
 func _on_timer_timeout():
 	# if the player didn't tap mosquito, emit life losing signal
 	emit_signal("not_tapped")
+	tapped_sound.play()
 	queue_free() # automatically disappears when not pressed by the player.
 
 func _on_area_2d_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		# Delete enemy instance then record score
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			# Write a function here to play a tapped sound effect
+			#sound.play()
 			# Send a signal for scoring
 			emit_signal("is_tapped")
 			# Remove enemy instance
--- _script/game_over.gd
+++ _script/game_over.gd
@@ -3,5 +3,8 @@ extends Control
 @onready var score_label = $Panel/VBoxContainer/Score
 @onready var globals = get_node("/root/Globals")
 
+func _start():
+	$BadMusic.play()
+
 func _process(delta):
 	score_label.text = "Tapped: " + str(globals.score)
